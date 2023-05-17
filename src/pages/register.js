368faed361c4488b423eab2341950c6bf52a3d6e
Anurag7275977 <arjunsharma705463@gmail.com>
2023-05-18 00:42:30 +0530

    Header with routing assignment completed

---
+++ src/pages/register.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+
+
+export default function Register(){
+    return (
+        <>
+        <Header/>
+            <h1>This is Register page</h1>
+        </>
+    )
+}
